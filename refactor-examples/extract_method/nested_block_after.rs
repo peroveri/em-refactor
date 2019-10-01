5d89bb00f4d392252209f786d4b126557c86ed91
Per Ove Ringdal <perori@uio.no>
2019-10-01 11:34:50 +0200

    fix wrong fn name in test

--- refactor-examples/extract_method/nested_block_after.rs
+++ refactor-examples/extract_method/nested_block_after.rs
@@ -1,10 +1,10 @@
-fn foo(i: &mut i32) {
+fn inc(i: &mut i32) {
     *i += 1;
 }
 fn main() {
     let mut i = 0;
     {
-        foo(&mut i);
+        inc(&mut i);
     }
     println!("{}", &i);
 }
