f54ab95078531545440923a10d194b3483b06cd8
Per Ove Ringdal <perori@uio.no>
2019-11-11 17:32:22 +0100

    struct pattern collector

--- refactor-lib/src/refactorings/box_field/mod.rs
+++ refactor-lib/src/refactorings/box_field/mod.rs
@@ -1,15 +1,51 @@
-use super::utils::{map_change_from_span};
+use super::utils::map_change_from_span;
 use crate::change::Change;
-use struct_field_collector::collect_field;
 use rustc::hir;
 use rustc::ty::TyCtxt;
+use struct_field_collector::collect_field;
 use syntax_pos::Span;
 
 use super::utils::get_source;
 
 mod binds_to_field_collector;
-mod struct_field_collector;
 mod function_body_collector;
+mod struct_field_collector;
+mod struct_pattern_collector;
+
+/// Box field refactoring
+/// 
+/// ## Algorithm
+/// 
+/// Input 
+/// - `tcx: Typed context`
+/// - `span: Byte range`
+/// 
+/// Steps
+/// - F <- the field which should be boxed
+/// - T <- the type of the struct (from Items -> StructStruct)
+/// - Ps <- All StructPattern where PathInExpression has type T and F is in StructPatternFields
+/// - Vs <- All StructExpressions where PathInExpression has type T and F is in StructExprFields
+/// - Fs <- All FieldExpressions where Expression has type T and F is IDENTIFIER
+/// - if |Ps| > 0 then abort
+/// - Change F's type in T to Box<>
+/// - for V in Vs:
+///   - Add Box::new around V
+/// - for F' in Fs
+///   - Add * around F'
+pub fn do_refactoring(tcx: TyCtxt, span: Span) -> Result<Vec<Change>, String> {
+    if let Some(field) = collect_field(tcx, span) {
+        let mut changes = get_use_changes(tcx, field);
+        changes.push(get_field_change(tcx, field));
+        Ok(changes)
+    } else {
+        Err(format!(
+            // do this on a higher level?
+            "{}:{} is not a valid selection!",
+            span.lo().0,
+            span.hi().0
+        ))
+    }
+}
 
 fn get_field_change(tcx: TyCtxt, field: &hir::StructField) -> Change {
     let type_as_str = hir::print::to_string(hir::print::NO_ANN, |s| s.print_type(&*field.ty));
@@ -27,10 +63,7 @@ fn get_write_change(tcx: TyCtxt, span: Span) -> String {
     format!("Box::new({})", original)
 }
 
-fn get_use_changes<'tcx>(
-    tcx: TyCtxt<'tcx>,
-    field: &hir::StructField,
-) -> Vec<Change> {
+fn get_use_changes<'tcx>(tcx: TyCtxt<'tcx>, field: &hir::StructField) -> Vec<Change> {
     let bodies = function_body_collector::collect_function_bodies(tcx);
     let (reads, writes) = binds_to_field_collector::run_on_all_bodies(
         tcx,
@@ -45,25 +78,9 @@ fn get_use_changes<'tcx>(
             .map(|read| map_change_from_span(tcx, *read, get_read_change(tcx, *read))),
     );
     r.extend(
-        writes.iter().map(|write| {
-            map_change_from_span(tcx, *write, get_write_change(tcx, *write))
-        }),
+        writes
+            .iter()
+            .map(|write| map_change_from_span(tcx, *write, get_write_change(tcx, *write))),
     );
     r
-}
-
-// translate span to (struct, field)
-// then, find all bindings to the field and update
-pub fn do_refactoring(tcx: TyCtxt, span: Span) -> Result<Vec<Change>, String> {
-
-    if let Some(field) = collect_field(tcx, span) {
-        let mut changes = get_use_changes(tcx, field);
-        changes.push(get_field_change(tcx, field));
-        Ok(changes)
-    } else {
-        Err(format!( // do this on a higher level?
-            "{}:{} is not a valid selection!",
-            span.lo().0, span.hi().0
-        ))
-    }
 }
---
+++ refactor-lib/src/refactorings/box_field/struct_pattern_collector.rs
@@ -0,0 +1,155 @@
+use rustc::hir::{
+    self,
+    intravisit::{walk_crate, NestedVisitorMap, Visitor},
+};
+use rustc::ty::TyCtxt;
+use syntax_pos::Span;
+
+///
+/// Collect all places where a given struct occurs in a pattern. Field_ident should also occur in the pattern.
+///
+pub fn collect_struct_patterns(
+    tcx: TyCtxt,
+    struct_hir_id: hir::HirId,
+    field_ident: String,
+) -> Vec<Span> {
+    let mut v = StructPatternCollector {
+        tcx,
+        struct_hir_id,
+        field: vec![],
+        field_ident,
+        body_id: None,
+    };
+
+    walk_crate(&mut v, tcx.hir().krate());
+
+    v.field
+}
+
+struct StructPatternCollector<'v> {
+    tcx: TyCtxt<'v>,
+    struct_hir_id: hir::HirId,
+    field: Vec<Span>,
+    field_ident: String,
+    body_id: Option<hir::BodyId>,
+}
+
+impl StructPatternCollector<'_> {
+    fn path_resolves_to_struct(&self, qpath: &hir::QPath, h: hir::HirId) -> bool {
+        let typecheck_table = self.tcx.typeck_tables_of(h.owner_def_id());
+
+        if let hir::QPath::Resolved(Some(ty), _) = qpath {
+            let qp_type = typecheck_table.node_type(ty.hir_id);
+            let struct_type = typecheck_table.node_type(self.struct_hir_id);
+            rustc::ty::TyS::same_type(struct_type, qp_type)
+        } else {
+            let res = typecheck_table.qpath_res(qpath, h);
+            let res_def_id = res.def_id();
+            res_def_id == self.struct_hir_id.owner_def_id()
+        }
+    }
+}
+
+impl<'v> Visitor<'v> for StructPatternCollector<'v> {
+    fn nested_visit_map<'this>(&'this mut self) -> NestedVisitorMap<'this, 'v> {
+        NestedVisitorMap::All(&self.tcx.hir())
+    }
+    fn visit_fn(
+        &mut self,
+        fk: hir::intravisit::FnKind<'v>,
+        fd: &'v hir::FnDecl,
+        body_id: hir::BodyId,
+        s: Span,
+        h: hir::HirId,
+    ) {
+        self.body_id = Some(body_id);
+        hir::intravisit::walk_fn(self, fk, fd, body_id, s, h);
+    }
+    fn visit_pat(&mut self, p: &'v hir::Pat) {
+        if let hir::PatKind::Struct(qpath, fields, _) = &p.kind {
+            if self.path_resolves_to_struct(qpath, p.hir_id) {
+                for fp in fields {
+                    if format!("{}", fp.ident) == self.field_ident {
+                        self.field.push(fp.span);
+                    }
+                }
+            }
+        }
+        hir::intravisit::walk_pat(self, p);
+    }
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+    use crate::{create_test_span, run_test};
+    use quote::quote;
+
+    fn create_program_match() -> quote::__rt::TokenStream {
+        quote! {
+            struct S { field: u32 }
+            fn foo() {
+                match (S {field: 0}) {
+                    S {field: 0} => {},
+                    _ => {}
+                }
+            }
+        }
+    }
+    fn create_program_match_without_field() -> quote::__rt::TokenStream {
+        quote! {
+            struct S { field: u32 }
+            fn foo() {
+                match (S {field: 0}) {
+                    S {..} => {},
+                    _ => {}
+                }
+            }
+        }
+    }
+
+    fn create_program_if_let() -> quote::__rt::TokenStream {
+        quote! {
+            struct S { field: u32 }
+            fn foo() {
+                if let (S{field: 0}) = (S{field: 1}) {
+                }
+            }
+        }
+    }
+    fn get_struct_hir_id(tcx: TyCtxt<'_>) -> hir::HirId {
+        let field =
+            super::super::struct_field_collector::collect_field(tcx, create_test_span(11, 16))
+                .unwrap();
+        let struct_def_id = field.hir_id.owner_def_id();
+        tcx.hir().as_local_hir_id(struct_def_id).unwrap()
+    }
+
+    #[test]
+    fn struct_pattern_collector_should_collect_match_pattern() {
+        run_test(create_program_match(), |tcx| {
+            let hir_id = get_struct_hir_id(tcx);
+            let fields = collect_struct_patterns(tcx, hir_id, "field".to_owned());
+
+            assert_eq!(fields.len(), 1);
+        });
+    }
+    #[test]
+    fn struct_pattern_collector_should_collect_if_let_pattern() {
+        run_test(create_program_if_let(), |tcx| {
+            let hir_id = get_struct_hir_id(tcx);
+            let fields = collect_struct_patterns(tcx, hir_id, "field".to_owned());
+
+            assert_eq!(fields.len(), 1);
+        });
+    }
+    #[test]
+    fn struct_pattern_collector_should_not_collect_struct_without_field() {
+        run_test(create_program_match_without_field(), |tcx| {
+            let struct_hir_id = get_struct_hir_id(tcx);
+            let fields = collect_struct_patterns(tcx, struct_hir_id, "field".to_owned());
+
+            assert_eq!(fields.len(), 0);
+        });
+    }
+}
