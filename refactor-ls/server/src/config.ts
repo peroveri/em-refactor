736b9071ac7aefad2e68e1b78937080f387b8997
Per Ove Ringdal <perori@uio.no>
2019-11-18 15:20:13 +0100

    update config in refactor-ls. TODO: the absolute path shouldnt be in git

--- refactor-ls/server/src/config.ts
+++ refactor-ls/server/src/config.ts
@@ -1,4 +1,4 @@
 const config = {
-    refactorToolManifestPath: '/home/perove/dev/github.uio.no/refactor-rust/Cargo.toml'
+    refactorToolManifestPath: '/home/perove/dev/github.uio.no/refactor-rust/refactor-lib/Cargo.toml'
 };
 export default config;
