99c5cc4e4bc13e704add2f80be34ed33595323fb
Per Ove Ringdal <perori@uio.no>
2020-01-10 14:24:45 +0100

    update test

--- refactor-lib/tests/data/box_field/struct_pattern_field_binding_1_after.rs
+++ refactor-lib/tests/data/box_field/struct_pattern_field_binding_1_after.rs
@@ -1,7 +1,9 @@
 struct S { field: Box<i32> }
 fn main() {
     match (S {field: Box::new(0)}) {
-        S {field: a} => {}, // new binding 'a'
+        S {field: a} => {
+            let _: i32 = (*a);
+        }, // new binding 'a'
         S {..} => {}, // Et Cetera
         S {field: _} => {}, // Wildcard
         _ => {}
