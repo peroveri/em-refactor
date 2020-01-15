e94f9417cda9819055586f8c198bc94fcf1b35fd
Per Ove Ringdal <perori@uio.no>
2020-01-15 09:18:40 +0100

    refactor / order tests

--- refactor-lib/tests/refactoring_extract_block_tests.rs
+++ refactor-lib/tests/refactoring_extract_block_tests.rs
@@ -7,26 +7,6 @@ fn run_test(name: &str) {
 }
 
 #[test]
-fn extract_block_1() {
-    run_test("extract_block_1");
-}
-#[test]
-fn extract_block_with_expr() {
-    run_test("extract_block_with_expr");
-}
-#[test]
-fn extract_block_mut() {
-    run_test("extract_block_mut");
-}
-#[test]
-fn extract_block_multiple_use() {
-    run_test("extract_block_multiple_use");
-}
-#[test]
-fn extract_block_field_used_later() {
-    run_test("extract_block_field_used_later");
-}
-#[test]
 #[ignore]
 fn extract_block_borrow_field() {
     run_test("borrow_field");
@@ -35,4 +15,24 @@ fn extract_block_borrow_field() {
 #[ignore]
 fn extract_block_deref() {
     run_test("deref");
+}
+#[test]
+fn extract_block_1() {
+    run_test("extract_block_1");
+}
+#[test]
+fn extract_block_field_used_later() {
+    run_test("extract_block_field_used_later");
+}
+#[test]
+fn extract_block_multiple_use() {
+    run_test("extract_block_multiple_use");
+}
+#[test]
+fn extract_block_mut() {
+    run_test("extract_block_mut");
+}
+#[test]
+fn extract_block_with_expr() {
+    run_test("extract_block_with_expr");
 }
