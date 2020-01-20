c9a9a33a366c3c1ffe7c3183cedde1742e3f8cb7
Per Ove Ringdal <perori@uio.no>
2020-01-20 10:10:23 +0100

    Syntax error in a test

--- refactor-lib/tests/data/box_field/attribute_macro_debug.rs
+++ refactor-lib/tests/data/box_field/attribute_macro_debug.rs
@@ -1,5 +1,5 @@
 #[derive(Debug)]
-struct S { field: i32 };
+struct S { field: i32 }
 fn main(){}
 // it should be possible to box a field when the struct has the Debug attribute
 // this is currently not implemented, so the test is ignored
--- refactor-lib/tests/data/box_field/attribute_macro_debug_after.rs
+++ refactor-lib/tests/data/box_field/attribute_macro_debug_after.rs
@@ -1,5 +1,5 @@
 #[derive(Debug)]
-struct S { field: Box<i32> };
+struct S { field: Box<i32> }
 fn main(){}
 // it should be possible to box a field when the struct has the Debug attribute
 // this is currently not implemented, so the test is ignored
