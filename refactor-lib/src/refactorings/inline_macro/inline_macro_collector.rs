19e7005df4d9bfd58137104fa0147130a1dd5ada
Per Ove Ringdal <perori@uio.no>
2020-01-23 12:15:03 +0100

    inline macro: refactor

---
+++ refactor-lib/src/refactorings/inline_macro/inline_macro_collector.rs
@@ -0,0 +1,45 @@
+use syntax::ast::{Crate, Expr, Stmt};
+use syntax::visit::{Visitor, walk_crate };
+use rustc_span::Span;
+
+
+pub fn collect_inline_macro(span: Span, crate_: &Crate) -> Option<(String, Span)> {
+    let mut v = MacroCollector {
+        span,
+        res: vec![],
+        res_span: None
+    };
+
+    walk_crate(&mut v, crate_);
+
+    if v.res.len() > 0 {
+        Some((v.res.join(""), v.res_span.unwrap()))
+    } else {
+        None
+    }
+}
+
+struct MacroCollector {
+    span: Span,
+    res: Vec<String>,
+    res_span: Option<Span>
+}
+
+impl<'ast> Visitor<'ast> for MacroCollector {
+    fn visit_expr(&mut self, ex: &'ast Expr) {
+        if ex.span.from_expansion() && self.span.overlaps(ex.span.source_callsite()) {
+            self.res.push(syntax::print::pprust::expr_to_string(ex));
+            self.res_span = ex.span.parent();
+        } else {
+            syntax::visit::walk_expr(self, ex);
+        }
+    }
+    fn visit_stmt(&mut self, stmt: &'ast Stmt) {
+        if stmt.span.from_expansion() && self.span.overlaps(stmt.span.source_callsite()) {
+            self.res.push(syntax::print::pprust::stmt_to_string(stmt));
+            self.res_span = stmt.span.parent();
+        } else {
+            syntax::visit::walk_stmt(self, stmt);
+        }
+    }
+}
--- refactor-lib/src/refactorings/inline_macro/mod.rs
+++ refactor-lib/src/refactorings/inline_macro/mod.rs
@@ -2,54 +2,21 @@ use rustc_span::Span;
 use crate::change::Change;
 use crate::refactor_definition::RefactoringError;
 use super::utils::map_change_from_span;
-use syntax::ast::{Expr, Stmt};
-use syntax::visit::{Visitor, walk_crate };
+use inline_macro_collector::collect_inline_macro;
+
+mod inline_macro_collector;
 
 pub fn do_refactoring<'tcx>(compiler: &rustc_interface::interface::Compiler,queries:  &'tcx rustc_interface::Queries<'tcx>, span: Span) -> Result<Vec<Change>, RefactoringError>{
 
-    let mut v = MacroCollector {
-        span,
-        res: vec![],
-        res_span: None
-    };
     let (crate_, ..) = 
     &*queries
         .expansion()
         .unwrap()
         .peek_mut();
 
-    walk_crate(&mut v, crate_);
-
-    if v.res.len() > 0 {
-        let r = v.res.join("");
-        return Ok(vec![map_change_from_span(compiler.source_map(), v.res_span.unwrap(), r)]);
+    if let Some((replacement, repl_span)) = collect_inline_macro(span, crate_) {
+        Ok(vec![map_change_from_span(compiler.source_map(), repl_span, replacement)])
     } else {
-
-    }
-    Err(RefactoringError::file_not_found(""))
-}
-
-struct MacroCollector {
-    span: Span,
-    res: Vec<String>,
-    res_span: Option<Span>
-}
-
-impl<'ast> Visitor<'ast> for MacroCollector {
-    fn visit_expr(&mut self, ex: &'ast Expr) {
-        if ex.span.from_expansion() && self.span.overlaps(ex.span.source_callsite()) {
-            self.res.push(syntax::print::pprust::expr_to_string(ex));
-            self.res_span = ex.span.parent();
-        } else {
-            syntax::visit::walk_expr(self, ex);
-        }
-    }
-    fn visit_stmt(&mut self, stmt: &'ast Stmt) {
-        if stmt.span.from_expansion() && self.span.overlaps(stmt.span.source_callsite()) {
-            self.res.push(syntax::print::pprust::stmt_to_string(stmt));
-            self.res_span = stmt.span.parent();
-        } else {
-            syntax::visit::walk_stmt(self, stmt);
-        }
+        Err(RefactoringError::invalid_selection(span.lo().0, span.hi().0))
     }
 }
