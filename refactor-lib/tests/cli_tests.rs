29fe4e9787bff7b71249f7bd59b85e2e5f0629c7
Per Ove Ringdal <perori@uio.no>
2020-01-24 15:17:25 +0100

    cli tests: sort by name

--- refactor-lib/tests/cli_tests.rs
+++ refactor-lib/tests/cli_tests.rs
@@ -6,25 +6,6 @@ use cli_tests_utils::*;
 mod cli_tests_utils;
 
 #[test]
-fn cli_should_display_help() {
-    cargo_my_refactor()
-        .arg("--help")
-        .assert()
-        .success()
-        .stdout(predicate::str::starts_with(
-            "Refactorings for the Rust programming language.",
-        ));
-}
-#[test]
-fn cli_should_display_version() {
-    cargo_my_refactor()
-        .arg("--version")
-        .assert()
-        .success()
-        .stdout(predicate::str::starts_with("Version:"));
-}
-
-#[test]
 fn cli_missing_args_should_output_nicely() {
     cargo_my_refactor()
         .arg(WORKSPACE_ARG)
@@ -36,79 +17,6 @@ fn cli_missing_args_should_output_nicely() {
         .assert()
         .failure()
         .stderr(predicate::str::starts_with("Expected --refactoring\n"));
-}
-
-#[test]
-fn cli_unknown_refactoring() {
-    cargo_my_refactor()
-        .arg(WORKSPACE_ARG)
-        .arg("--refactoring=invalid_refactoring_name")
-        .arg("--")
-        .arg(format!(
-            "--target-dir={}",
-            create_tmp_dir().path().to_str().unwrap()
-        ))
-        .assert()
-        .failure()
-        .stderr(predicate::str::starts_with(
-            "Unknown refactoring: invalid_refactoring_name\n",
-        ));
-}
-
-#[test]
-fn cli_output_json() {
-    let replacement = FileReplaceContent {
-        byte_end: 40,
-        byte_start: 16,
-        char_end: 28,
-        char_start: 4,
-        file_name: "src/main.rs".to_owned(),
-        line_end: 1,
-        line_start: 1,
-        replacement: "let s = \n{\nlet s = \"Hello, world!\";\ns};".to_owned(),
-    };
-    let expected = vec![
-        create_output("hello_world", false, &replacement),
-        create_output("hello_world", true, &replacement),
-    ];
-
-    let actual = cargo_my_refactor()
-        .arg(WORKSPACE_ARG)
-        .arg("--output-replacements-as-json")
-        .arg("--refactoring=extract-block")
-        .arg("--selection=16:40")
-        .arg("--file=src/main.rs")
-        .arg("--")
-        .arg(format!(
-            "--target-dir={}",
-            create_tmp_dir().path().to_str().unwrap()
-        ))
-        .output().unwrap();
-    
-    assert_json_eq(expected, actual);
-}
-
-#[test]
-fn cli_provide_type() {
-    let expected = format!("{}\n{}\n", json!([{
-        "type": "fn foo(i32,u32) -> (i32)"
-    }]), json!([{
-        "type": "fn foo(i32,u32) -> (i32)"
-    }]));
-
-    cargo_my_refactor()
-        .arg(WORKSPACE_ARG)
-        .arg("--provide-type")
-        .arg("--selection=72:72")
-        .arg("--file=src/main.rs")
-        .arg("--")
-        .arg(format!(
-            "--target-dir={}",
-            create_tmp_dir().path().to_str().unwrap()
-        ))
-        .assert()
-        .success()
-        .stdout(expected);
 }
 
 #[test]
@@ -176,4 +84,96 @@ fn cli_multiroot_project_main() {
         )).output().unwrap();
 
     assert_json_eq(expected, actual);
+}
+
+#[test]
+fn cli_output_json() {
+    let replacement = FileReplaceContent {
+        byte_end: 40,
+        byte_start: 16,
+        char_end: 28,
+        char_start: 4,
+        file_name: "src/main.rs".to_owned(),
+        line_end: 1,
+        line_start: 1,
+        replacement: "let s = \n{\nlet s = \"Hello, world!\";\ns};".to_owned(),
+    };
+    let expected = vec![
+        create_output("hello_world", false, &replacement),
+        create_output("hello_world", true, &replacement),
+    ];
+
+    let actual = cargo_my_refactor()
+        .arg(WORKSPACE_ARG)
+        .arg("--output-replacements-as-json")
+        .arg("--refactoring=extract-block")
+        .arg("--selection=16:40")
+        .arg("--file=src/main.rs")
+        .arg("--")
+        .arg(format!(
+            "--target-dir={}",
+            create_tmp_dir().path().to_str().unwrap()
+        ))
+        .output().unwrap();
+    
+    assert_json_eq(expected, actual);
+}
+
+#[test]
+fn cli_provide_type() {
+    let expected = format!("{}\n{}\n", json!([{
+        "type": "fn foo(i32,u32) -> (i32)"
+    }]), json!([{
+        "type": "fn foo(i32,u32) -> (i32)"
+    }]));
+
+    cargo_my_refactor()
+        .arg(WORKSPACE_ARG)
+        .arg("--provide-type")
+        .arg("--selection=72:72")
+        .arg("--file=src/main.rs")
+        .arg("--")
+        .arg(format!(
+            "--target-dir={}",
+            create_tmp_dir().path().to_str().unwrap()
+        ))
+        .assert()
+        .success()
+        .stdout(expected);
+}
+
+#[test]
+fn cli_should_display_help() {
+    cargo_my_refactor()
+        .arg("--help")
+        .assert()
+        .success()
+        .stdout(predicate::str::starts_with(
+            "Refactorings for the Rust programming language.",
+        ));
+}
+#[test]
+fn cli_should_display_version() {
+    cargo_my_refactor()
+        .arg("--version")
+        .assert()
+        .success()
+        .stdout(predicate::str::starts_with("Version:"));
+}
+
+#[test]
+fn cli_unknown_refactoring() {
+    cargo_my_refactor()
+        .arg(WORKSPACE_ARG)
+        .arg("--refactoring=invalid_refactoring_name")
+        .arg("--")
+        .arg(format!(
+            "--target-dir={}",
+            create_tmp_dir().path().to_str().unwrap()
+        ))
+        .assert()
+        .failure()
+        .stderr(predicate::str::starts_with(
+            "Unknown refactoring: invalid_refactoring_name\n",
+        ));
 }
