8342495f7e8bdcff96cb452ec672a82e041a6a34
Per Ove Ringdal <perori@uio.no>
2020-02-18 11:57:49 +0100

    fix order

--- refactor-lib/tests/refactoring_extract_block_grammar_tests.rs
+++ refactor-lib/tests/refactoring_extract_block_grammar_tests.rs
@@ -11,12 +11,12 @@ fn extract_block_grammar_expr_and_stmt_2() {
     run_test("expr_and_stmt_2");
 }
 #[test]
-fn extract_block_grammar_expr_and_stmt_3() {
-    run_test("expr_and_stmt_3");
-}
-#[test]
 fn extract_block_grammar_expr_and_stmt_23() {
     run_test("expr_and_stmt_23");
+}
+#[test]
+fn extract_block_grammar_expr_and_stmt_3() {
+    run_test("expr_and_stmt_3");
 }
 #[test]
 fn extract_block_grammar_single_expr() {
