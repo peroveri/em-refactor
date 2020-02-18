ca9a3228b9b95df774ead84b8719adb0d29d3ff8
Per Ove Ringdal <perori@uio.no>
2020-02-18 11:31:45 +0100

    rename

--- refactor-lib/tests/refactoring_extract_block_grammar_tests.rs
+++ refactor-lib/tests/refactoring_extract_block_grammar_tests.rs
@@ -7,10 +7,6 @@ fn run_test(name: &str) {
 }
 
 #[test]
-fn extract_block_grammar_single_expr() {
-    run_test("single_expr");
-}
-#[test]
 #[ignore]
 fn extract_block_grammar_expr_and_stmt_2() {
     run_test("expr_and_stmt_2");
@@ -22,4 +18,8 @@ fn extract_block_grammar_expr_and_stmt_3() {
 #[test]
 fn extract_block_grammar_expr_and_stmt_23() {
     run_test("expr_and_stmt_23");
+}
+#[test]
+fn extract_block_grammar_single_expr() {
+    run_test("single_expr");
 }
