f6c72ba7c57d7d2f07e59e470a5ab1c6e972ede2
Per Ove Ringdal <perori@uio.no>
2020-02-28 16:46:52 +0100

    comment: maybe remove tests from candidates

--- refactor-lib/tests/experiments.rs
+++ refactor-lib/tests/experiments.rs
@@ -4,5 +4,6 @@ mod exp;
 #[ignore]
 fn experiments_run_tests() -> std::io::Result<()> {
     // exp::run_all_exp("box-named-field")
+    // TODO: we probably don't want to query candidates for the tests
     exp::run_all_exp("extract-block")
 }
