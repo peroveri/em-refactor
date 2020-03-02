e730f1653f6eca0a880f9f7f80796efef9f63a94
Per Ove Ringdal <perori@uio.no>
2020-03-02 11:06:37 +0100

    rename

--- refactor-lib/tests/refactoring_convert_closure_to_local_function.rs
+++ refactor-lib/tests/refactoring_convert_closure_to_local_function.rs
@@ -8,6 +8,6 @@ fn run_test(name: &str) {
 
 #[test]
 #[ignore]
-fn convert_closure_to_local_function_fn_with_no_annotations() {
-    run_test("fn_with_no_annotations");
+fn convert_closure_to_local_function_no_annotations() {
+    run_test("no_annotations");
 }
