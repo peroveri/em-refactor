51f6f90c10ec5dd1d4bf58d90d5170f3047c19db
Per Ove Ringdal <perori@uio.no>
2020-03-02 11:12:45 +0100

    convert closure to local function: add test

---
+++ refactor-lib/tests/data/convert_closure_to_local_function/annotate_param.rs
@@ -0,0 +1,6 @@
+fn main() {
+    fn main() {
+        let i = 1;
+        (|i: _|{let _: i32 = i;})(i);
+    }
+}
---
+++ refactor-lib/tests/data/convert_closure_to_local_function/annotate_param_after.rs
@@ -0,0 +1,6 @@
+fn main() {
+    fn main() {
+        let i = 1;
+        ({fn foo (i: i32) {let _: i32 = i;} foo})(i);
+    }
+}
