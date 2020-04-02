cd3dc795e355228a8cb51e76f0fffec72c4161c6
Per Ove Ringdal <perori@uio.no>
2020-04-02 13:51:21 +0200

    Comment

--- refactor-lib/src/refactorings/extract_block/expr_use_visit.rs
+++ refactor-lib/src/refactorings/extract_block/expr_use_visit.rs
@@ -60,8 +60,8 @@ impl<'a, 'tcx> Delegate<'tcx> for VariableCollectorDelegate<'tcx> {
     }
 }
 
-/// Collect variables declared inside 'span', but used outside 'span'
-/// Should also return an error if one of the return values is a borrow, or contains a borrow
+/// Vs <- Collect variables declared inside 'span', but used outside 'span'
+/// If one of Vs is a borrow or contains a borrow (struct, tuple type, etc.), then we should return an error
 pub fn collect_vars(tcx: &TyContext, body_id: BodyId, span: Span) -> VariableUseCollection {
     let def_id = body_id.hir_id.owner.to_def_id();
     tcx.0.infer_ctxt().enter(|inf| {
