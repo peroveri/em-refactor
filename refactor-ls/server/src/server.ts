b0a7af3512805ed6a384af5185979cd7259d8418
Per Ove Ringdal <perori@uio.no>
2020-04-23 08:09:58 +0200

    ls: show notification

--- refactor-ls/server/src/server.ts
+++ refactor-ls/server/src/server.ts
@@ -3,6 +3,7 @@ import "reflect-metadata";
 import { bootstrapAndReturnConnection } from './services/services';
 import { container } from "tsyringe";
 import { ExecuteCommandService } from "./services/ExecuteCommandService";
+import { NotificationService } from "./services/NotificationService";
 
 // don't create a connection if we're testing
 // should perhaps use mocks instead
@@ -11,4 +12,6 @@ if(typeof global.it !== 'function') {
     const connection = bootstrapAndReturnConnection();
     
     connection.onExecuteCommand(container.resolve(ExecuteCommandService).handleExecuteCommand);
+
+    container.resolve(NotificationService).sendInfoNotification("Language server started");
 }
