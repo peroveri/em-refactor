343ca921f342c816136000f2662e5edcfa36e605
Per Ove Ringdal <perori@uio.no>
2020-05-04 09:49:44 +0200

    box field: refactor

--- refactor-lib/src/refactorings/box_named_field/mod.rs
+++ refactor-lib/src/refactorings/box_named_field/mod.rs
@@ -1,7 +1,6 @@
 use rustc_hir::{HirId, Item, ItemKind};
 use rustc_span::Span;
 
-use crate::refactorings::utils::{get_source, map_change_from_span};
 use crate::refactoring_invocation::{AstDiff, QueryResult, RefactoringErrorInternal, TyContext};
 use super::visitors::{collect_local_variable_use, collect_struct_field_access_expressions};
 use struct_expression_collector::collect_struct_expressions;
@@ -17,35 +16,32 @@ pub fn do_refactoring(tcx: &TyContext, struct_hir_id: HirId, field_ident: &str, 
     if !struct_patterns.other.is_empty() {
         return Err(RefactoringErrorInternal::used_in_pattern(&field_ident));
     }
-    let source_map = tcx.0.sess.source_map();
-    
-    let mut changes = vec![map_change_from_span(
-        source_map,
+    let mut changes = vec![tcx.map_change(
         field_ty_span,
-        format!("Box<{}>", get_source(tcx.0, field_ty_span)),
+        format!("Box<{}>", tcx.get_source(field_ty_span)),
     )];
 
     let (struct_expressions, struct_expression_shorthands) = collect_struct_expressions(tcx, struct_hir_id, field_ident)?;
 
     for struct_expression in struct_expressions {
-        let replacement = format!("Box::new({})", get_source(tcx.0, struct_expression));
-        changes.push(map_change_from_span(source_map, struct_expression, replacement));
+        let replacement = format!("Box::new({})", tcx.get_source(struct_expression));
+        changes.push(tcx.map_change(struct_expression, replacement));
     }
 
     for (struct_expression, ident) in struct_expression_shorthands {
-        let replacement = format!("{}: Box::new({})", ident, get_source(tcx.0, struct_expression));
-        changes.push(map_change_from_span(source_map, struct_expression, replacement));
+        let replacement = format!("{}: Box::new({})", ident, tcx.get_source(struct_expression));
+        changes.push(tcx.map_change(struct_expression, replacement));
     }
 
     for field_access_expression in collect_struct_field_access_expressions(tcx.0, struct_hir_id, field_ident) {
-        let replacement = format!("(*{})", get_source(tcx.0, field_access_expression));
-        changes.push(map_change_from_span(source_map, field_access_expression, replacement));
+        let replacement = format!("(*{})", tcx.get_source(field_access_expression));
+        changes.push(tcx.map_change(field_access_expression, replacement));
     }
 
     for new_binding in struct_patterns.new_bindings {
         for local_use in collect_local_variable_use(tcx.0, new_binding) {
-            let replacement = format!("(*{})", get_source(tcx.0, local_use));
-            changes.push(map_change_from_span(source_map, local_use, replacement));
+            let replacement = format!("(*{})", tcx.get_source(local_use));
+            changes.push(tcx.map_change(local_use, replacement));
         }
     }
 
