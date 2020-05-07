8838c00394a09714a008194e48388daabc1964e0
Per Ove Ringdal <perori@uio.no>
2020-05-07 08:59:08 +0200

    fix warning

--- refactor-lib/src/refactorings/close_over_variables/variable_use_collection.rs
+++ refactor-lib/src/refactorings/close_over_variables/variable_use_collection.rs
@@ -114,11 +114,11 @@ impl VariableUseCollection {
 
         ids.iter()
             .map(|id| {
-                let mut v = map.get(id).unwrap().clone();
+                map.get(id).unwrap().clone()
                 // if v.is_borrow && !v.is_mut {
                 //     v.is_borrow = false;
                 // }
-                v
+                
             })
             .collect::<Vec<_>>()
     }
