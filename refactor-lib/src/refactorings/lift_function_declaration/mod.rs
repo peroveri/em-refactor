bd871c74d166189982d888fd6cd8fee6bb4ef095
Per Ove Ringdal <perori@uio.no>
2020-05-16 11:20:58 +0200

    comment

--- refactor-lib/src/refactorings/lift_function_declaration/mod.rs
+++ refactor-lib/src/refactorings/lift_function_declaration/mod.rs
@@ -15,6 +15,14 @@ pub fn do_refactoring(tcx: &TyContext, span: Span, _add_comment: bool) -> QueryR
     // Find target (is it module or impl?)
     let fn_def = collect_function_definition(tcx, span)?;
 
+    // if inside impl:
+    //   move to impl, convert call_expr, maybe convert to method
+    // else if inside impl_for:
+    //   move to impl, convert call_expr, maybe create impl, maybe convert to method
+    //   if create impl then we may have to add lifetime and gen args for impl, so skip this?
+    // else:
+    //   move to parent mod
+
     let changes = vec![
         tcx.map_change(fn_def.span, "".to_owned())?,
         tcx.map_change(fn_def.get_parent_mod_inner().shrink_to_hi(), format!("\n{}", tcx.get_source(fn_def.span)))?
