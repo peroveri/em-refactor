804c5e20237b14bd44bc66d71881ded46d05ee00
Per Ove Ringdal <perori@uio.no>
2020-05-25 17:06:46 +0200

    refactor: pub -> pub(crate)

--- em-refactor-lib/src/refactoring_invocation/ty_context.rs
+++ em-refactor-lib/src/refactoring_invocation/ty_context.rs
@@ -1,4 +1,4 @@
-use rustc_hir::{BodyId, StructField, HirId};
+use rustc_hir::{StructField, HirId};
 use rustc_middle::ty::TyCtxt;
 use rustc_span::Span;
 use em_refactor_lib_types::FileStringReplacement;
@@ -7,25 +7,22 @@ use super::{QueryResult, SourceMapContext};
 pub struct TyContext<'a>(pub TyCtxt<'a>);
 
 impl<'a> TyContext<'a> {
-    pub fn new(ty: TyCtxt<'a>) -> Self {
+    pub(crate) fn new(ty: TyCtxt<'a>) -> Self {
         Self(ty)
     }
-    pub fn source(&self) -> SourceMapContext<'a> {
+    pub(crate) fn source(&self) -> SourceMapContext<'a> {
         SourceMapContext {
             source_map: self.0.sess.source_map()
         }
     }
-    pub fn get_source(&self, span: Span) -> String {
+    pub(crate) fn get_source(&self, span: Span) -> String {
         self.source().get_source(span)
     }
-    pub fn map_change(&self, span: Span, replacement: String) -> QueryResult<FileStringReplacement> {
+    pub(crate) fn map_change(&self, span: Span, replacement: String) -> QueryResult<FileStringReplacement> {
         map_change_from_span(self.0.sess.source_map(), span, replacement)
     }
-    pub fn get_body_span(&self, body_id: BodyId) -> Span {
-        self.0.hir().body(body_id).value.span
-    }
 
-    pub fn get_struct_hir_id(&self, field: &StructField) -> HirId {
+    pub(crate) fn get_struct_hir_id(&self, field: &StructField) -> HirId {
         let struct_def_id = field.hir_id.owner.to_def_id();
         self.0.hir().as_local_hir_id(struct_def_id).unwrap()
     }
