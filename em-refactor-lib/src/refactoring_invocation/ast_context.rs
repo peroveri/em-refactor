31d243d0d2076aa67a9b4b98eeca91188df370dc
Per Ove Ringdal <perori@uio.no>
2020-05-25 17:07:50 +0200

    refactor: pub -> pub(crate)

--- em-refactor-lib/src/refactoring_invocation/ast_context.rs
+++ em-refactor-lib/src/refactoring_invocation/ast_context.rs
@@ -14,35 +14,35 @@ pub struct AstContext<'a, 'b> {
 }
 
 impl<'a, 'b> AstContext<'a, 'b> {
-    pub fn source(&self) -> SourceMapContext<'a> {
+    pub(crate) fn source(&self) -> SourceMapContext<'a> {
         SourceMapContext {
             source_map: self.compiler.source_map()
         }
     }
-    pub fn get_source_map(&self) -> &SourceMap {
+    pub(crate) fn get_source_map(&self) -> &SourceMap {
         self.compiler.session().source_map()
     }
-    pub fn get_source(&self, span: Span) -> String {
+    pub(crate) fn get_source(&self, span: Span) -> String {
         self.source().get_source(span)
     }
 
-    pub fn map_change(&self, span: Span, replacement: String) -> QueryResult<FileStringReplacement> {
+    pub(crate) fn map_change(&self, span: Span, replacement: String) -> QueryResult<FileStringReplacement> {
         map_change_from_span(self.compiler.source_map(), span, replacement)
     }
-    pub fn load_crate(&mut self) {
+    pub(crate) fn load_crate(&mut self) {
         let crate_ = self.queries
         .expansion()
         .unwrap()
         .peek().0.clone();
         self.crate_ = Some(crate_);
     }
-    pub fn get_crate(&self) -> &Crate {
+    pub(crate) fn get_crate(&self) -> &Crate {
         match &self.crate_ {
             Some(c) => c,
             _ => panic!()
         }
     }
-    pub fn new(compiler: &'a Compiler, queries: &'b Queries<'b>) -> Self {
+    pub(crate) fn new(compiler: &'a Compiler, queries: &'b Queries<'b>) -> Self {
         Self {
             compiler,
             queries,
